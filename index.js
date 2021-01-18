e1c26b41a3e95939c878badabe88ee9bb6c43c8e
Arianaomi <arianaomi.lp@gmail.com>
2021-01-18 03:54:33 -0600

    add port

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const dbConnect = require('./src/lib/db')
 
 const server = require('./src/server')
 
+const port = process.env.PORT || 8080
+
 dbConnect()
   .then(() => {
     console.log('DB connected')
-    server.listen(8080, () => {
+    server.listen(port, () => {
       console.log('server is listening')
     })
   })
